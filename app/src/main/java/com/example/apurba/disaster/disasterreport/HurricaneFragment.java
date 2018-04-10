f15de1e57beee92e7dcf359799b1a01ff7680102
Apurba Biswas <apurba10024@gmail.com>
2018-04-10 15:00:46 +0600

    4.1 Added Loading indecator with progress

--- app/src/main/java/com/example/apurba/disaster/disasterreport/HurricaneFragment.java
+++ app/src/main/java/com/example/apurba/disaster/disasterreport/HurricaneFragment.java
@@ -8,12 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HurricaneFragment extends Fragment {
+
 
 
     public HurricaneFragment() {
@@ -25,21 +27,37 @@ public class HurricaneFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.hurricane_fragment, container, false);
 
+        ProgressBar loading_indicator = rootView.findViewById(R.id.loading_spinner);
+
         WebView wv = (WebView) rootView.findViewById(R.id.webView);
-        wv.setWebViewClient(new MyBrowser());
+        wv.setWebViewClient(new MyBrowser(loading_indicator));
         wv.getSettings().setLoadsImagesAutomatically(true);
         wv.getSettings().setJavaScriptEnabled(true);
         wv.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         wv.loadUrl("http://www.thehurricanetracker.org/live-tracker");
 
+
         return rootView;
     }
 
     private class MyBrowser extends WebViewClient {
+        private ProgressBar loading_indicator;
+
+        public MyBrowser(ProgressBar progressBar){
+            this.loading_indicator=progressBar;
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            loading_indicator.setVisibility(View.GONE);
         }
     }
 
