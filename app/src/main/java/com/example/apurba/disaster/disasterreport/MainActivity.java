4423be9e075e083bbd2fe7ac8d17cb98baf69b43
Apurba Biswas <apurba10024@gmail.com>
2018-04-24 04:57:42 +0600

    5.8 Added Final design in main Activity

--- app/src/main/java/com/example/apurba/disaster/disasterreport/MainActivity.java
+++ app/src/main/java/com/example/apurba/disaster/disasterreport/MainActivity.java
@@ -4,10 +4,12 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapse_toolbar);
+        collapsingToolbar.setTitle("Disaster");
+
 
 
         ViewPager viewPager = findViewById(R.id.viewpager);
