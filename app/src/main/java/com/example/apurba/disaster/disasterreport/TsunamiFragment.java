a5cfca8a28f308dfce8f9b410c9c607f63bf4289
Apurba Biswas <apurba10024@gmail.com>
2018-04-23 20:32:26 +0600

    5.3 Ready To update the final design with coordinates Layout

--- app/src/main/java/com/example/apurba/disaster/disasterreport/TsunamiFragment.java
+++ app/src/main/java/com/example/apurba/disaster/disasterreport/TsunamiFragment.java
@@ -18,7 +18,6 @@ public class TsunamiFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tsunami_fragment, container, false);
