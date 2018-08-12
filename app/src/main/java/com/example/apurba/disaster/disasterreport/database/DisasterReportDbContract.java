b43d8faac9d1f4e3b664eab91754b90035ca8660
Apurba Biswas <apurba10024@gmail.com>
2018-08-12 06:27:05 +0600

    8.1 Prepared Databse operation files

---
+++ app/src/main/java/com/example/apurba/disaster/disasterreport/database/DisasterReportDbContract.java
@@ -0,0 +1,38 @@
+package com.example.apurba.disaster.disasterreport.database;
+
+/*
+ * Created by Apurba on 8/12/2018.
+ */
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class DisasterReportDbContract {
+
+    private DisasterReportDbContract(){
+    }
+
+    public static final String CONTENT_AUTHORITY = "com.example.apurba.disaster.disasterreport";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" +CONTENT_AUTHORITY);
+    public static final String PATH_EARTHQUAKE = "earthquake";
+
+
+    public static final class EarthQuakeEntry implements BaseColumns{
+        public static final String TABLE_NAME = "earthquake";
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_EARTHQUAKE);
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_E_ID = "e_id";
+        public static final String COLUMN_LOCATION = "location";
+        public static final String COLUMN_GEO_LOCATION = "geo_location";
+        public static final String COLUMN_MAGNITUDE = "magnitude";
+        public static final String COLUMN_TIME = "time";
+
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_EARTHQUAKE;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.ANY_CURSOR_ITEM_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_EARTHQUAKE;
+    }
+}
---
+++ app/src/main/java/com/example/apurba/disaster/disasterreport/database/DisasterReportDbHelper.java
@@ -0,0 +1,51 @@
+package com.example.apurba.disaster.disasterreport.database;
+
+/*
+ * Created by Apurba on 8/11/2018.
+ */
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import com.example.apurba.disaster.disasterreport.database.DisasterReportDbContract.EarthQuakeEntry;
+
+public class DisasterReportDbHelper extends SQLiteOpenHelper{
+
+    private static final String LOG_TAG = DisasterReportDbHelper.class.getSimpleName();
+    private static final String DATABASE_NAME = "DisasterReport.db";
+    private static final int DATABASE_VERSION = 1; // database version
+
+    private Context context;
+
+    public DisasterReportDbHelper(Context context){
+        //super(context,DATABASE_NAME,null,DATABASE_VERSION);
+        super(context, DATABASE_NAME,null ,DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        String SQL_CREATE_EARTHQUAKE_TABLE = "CREATE TABLE " + EarthQuakeEntry.TABLE_NAME + " ( " +
+                EarthQuakeEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                EarthQuakeEntry.COLUMN_E_ID + "TEXT PRIMARY KEY, " +
+                EarthQuakeEntry.COLUMN_LOCATION + "TEXT NOT NULL, " +
+                EarthQuakeEntry.COLUMN_GEO_LOCATION + "TEXT NOT NULL, " +
+                EarthQuakeEntry.COLUMN_MAGNITUDE + "TEXT NOT NULL, " +
+                EarthQuakeEntry.COLUMN_TIME + "TEXT NOT NULL);";
+        try{
+            sqLiteDatabase.execSQL(SQL_CREATE_EARTHQUAKE_TABLE);
+        }catch (Exception e){
+            Toast.makeText(context, "Error - SQL-Create-Table-Query: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        //
+    }
+
+}
